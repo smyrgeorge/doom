11522f7d246b4dabe47b98f00c839b36e4530265
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2026-01-07 00:32:22 +0100

    Updated config to the latest doom

--- config.el
+++ config.el
@@ -5,41 +5,62 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "George S."
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Yorgos S."
       user-mail-address "smyrgeorge@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-monokai-pro)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/dev/emacs/org/")
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type t)
 
-;; Here are some additional functions/macros that could help you configure Doom:
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/dev/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
-;; - `after!' for running code after a ppackage has loaded
+;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
@@ -48,6 +69,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
@@ -56,7 +79,6 @@
 (setq default-directory "~")
 
 ;; CUSTOM KEY BINDINGS
-(map! "s-b" #'+lookup/documentation)
 (map! "M-[" #'previous-buffer)
 (map! "M-]" #'next-buffer)
 (map! "s-[" #'better-jumper-jump-backward)
@@ -68,74 +90,9 @@
 ;; EVIL
 (add-hook! 'after-save-hook #'evil-normal-state)
 
-;; FONT-FACE
-;; Download fonts from https://www.jetbrains.com/lp/mono/
-;; For mac users copy font to 'ls ~/Library/Fonts'.
-;; NOTE: emacs wil crash if fonts not in path.
-(setq doom-font (font-spec :family "JetBrains Mono" :size 14)
-      doom-big-font (font-spec :family "JetBrains Mono" :size 20))
-
 ;; PROJECTILE
 ;; Discover projects using 'projectile-discover-projects-in-search-path'
-(setq projectile-project-search-path '("~/dev/projects/bevica"
-                                       "~/dev/projects/rust"))
-;; Disable projectile auto add projects.
-(setq projectile-track-known-projects-automatically nil)
-
-;; COMPANY
-(after! company
-  (setq company-idle-delay 0.1
-        company-minimum-prefix-length 1
-        company-tooltip-maximum-width 500)
-  (add-hook! 'evil-normal-state-entry-hook #'company-abort))
-
-;; LSP
-;; (setq lsp-signature-render-documentation nil)
-
-;; FORMAT
-;; Disable re-format with lsp. Use doom's format module (see init.el).
-;; (setq +format-with-lsp nil)
-
-;; MAGIT
-;; Enable delta diff by default.
-;; NOTE: if delta is not install, emacs may brake (or magit).
-(use-package magit-delta
-  :hook (magit-mode . magit-delta-mode))
-
-;; ELFEED :: RSS
-(setq elfeed-feeds
-      '("https://this-week-in-rust.org/rss.xml"
-        "https://blog.rust-lang.org/feed.xml"
-        "https://godotengine.org/rss.xml"
-        "https://rust-analyzer.github.io/feed.xml"
-        "https://sachachua.com/blog/category/emacs-news/feed"))
-
-;; RAINBOW-MODE
-;; hl-line-mode overrides the color highlighting of rainbow-mode,
-;; limiting the use of that plugin and on-site color changes using kurecolor.
-;; To automatically disable it only when rainbow-mode is active, you can add the following hook:
-(add-hook! 'rainbow-mode-hook
-  (hl-line-mode (if rainbow-mode -1 +1)))
-
-;; YASCROLL :: Yet another scrollbar
-;; (global-yascroll-bar-mode 1)
-
-;; PYTHON
-;; Set the anaconda home directory.
-(setq conda-anaconda-home "/usr/local/anaconda3/")
-
-;; TRAMP
-;; The following config tries to speed up tramp session over ssh.
-;; https://github.com/hlissner/doom-emacs/issues/3909
-(after! tramp
-  (setq tramp-inline-compress-start-size 1000
-        tramp-copy-size-limit 10000
-        vc-handled-backends '(Git)
-        tramp-verbose 1
-        tramp-default-method "scp"
-        tramp-use-ssh-controlmaster-options nil
-        projectile--mode-line "Projectile"
-        tramp-verbose 1))
+(setq projectile-project-search-path '("~/dev"))
 
 ;; UNIQUIFY
 ;; Meaningful names for buffers with the same name.
--- packages.el
+++ packages.el
@@ -1,84 +1,54 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
+;; To install a package:
+;;
+;;   1. Declare them here in a `package!' statement,
+;;   2. Run 'doom sync' in the shell,
+;;   3. Restart Emacs.
+;;
+;; Use 'C-h f package\!' to look up documentation for the `package!' macro.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+;; (package! some-package)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+;; https://github.com/radian-software/straight.el#the-recipe-format
+;; (package! another-package
+;;   :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+;; (package! this-package
+;;   :recipe (:host github :repo "username/repo"
+;;            :files ("some-file.el" "src/lisp/*.el")))
 
 ;; If you'd like to disable a package included with Doom, you can do so here
 ;; with the `:disable' property:
-;(package! builtin-package :disable t)
+;; (package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
 ;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+;; (package! builtin-package :recipe (:nonrecursive t))
+;; (package! builtin-package-2 :recipe (:repo "myfork/package"))
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+;; our package manager can't deal with; see radian-software/straight.el#279)
+;; (package! builtin-package :recipe (:branch "develop"))
 
 ;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
+;; (package! builtin-package :pin "1a2b3c4d5e")
 
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
+;; (unpin! pinned-package)
 ;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
+;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
-
-;; UNPIN ;;
-(unpin! lsp-mode lsp-ui consult-lsp)
-(unpin! rustic racer)
-(unpin! sbt-mode scala-mode lsp-metals)
-(unpin! yaml-mode)
-
-;; CUSTOM PACKAGES ;;
-
-;; Emacs plugin for livedown (live preview .md files in browser).
-;; This is an alternative for grip mode (in case API limit is reached).
-;; https://github.com/shime/emacs-livedown
-;; npm install -g livedown
-;; NOTE: Emacs may crash if livedown is not present.
-(package! emacs-livedown
-  :recipe (:host github :repo "shime/emacs-livedown"
-          :files ("livedown.el")))
-
-;; Emacs plugin for dlta (ultimate diff tool).
-;; https://github.com/dandavison/magit-delta
-;; brew install git-delta
-(package! magit-delta)
-
-;; yascroll :: Yet Another Scroll Bar Mode for GNU Emacs.
-;; https://github.com/emacsorphanage/yascroll
-;; (package! yascroll)
-
-;; kubernetes :: Emacs extension for controlling Kubernetes.
-;; https://github.com/abrochard/kubel
-;; https://github.com/kubernetes-el/kubernetes-el
-;; (package! kubel)
-;; (package! kubel-evil)
-;; (package! kubernetes)
-;; (package! kubernetes-evil)
+;; (unpin! t)
